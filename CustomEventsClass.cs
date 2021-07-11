d8e4399d9ccd1aaca711c01c5caa0e4e733e6b05
kranthi kumar <kranthikumar88@hotmail.com>
2021-07-11 18:17:22 +0530

    Add project files.

---
+++ CustomEventsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtmecsSample
+{
+    public class CustomEventsClass
+    {
+        public async void RunActions(IEnumerable<Action> actions)
+        {
+            foreach (var action in actions)
+            {
+                await Task.Delay(1200);
+                await Task.Run(action);
+                Console.WriteLine($"Completed on thread:{Thread.CurrentThread.ManagedThreadId}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtmecsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventsList = new List<Action>();
+            eventsList.Add(() =>
+            {
+                Console.WriteLine("Action 1");
+            });
+            eventsList.Add(() =>
+            {
+                Console.WriteLine("Action 2");
+            });
+            eventsList.Add(() =>
+            {
+                Console.WriteLine("Action 3");
+            });
+
+            var obj = new CustomEventsClass();
+            obj.RunActions(eventsList);
+
+            Console.ReadKey();
+        }
+    }
+}
